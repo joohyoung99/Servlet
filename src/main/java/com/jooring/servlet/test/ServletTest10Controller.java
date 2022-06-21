eef28a378c74be634c569c804711f4a87259faa7
303-18\megait <lecture@hagulu.com>
2022-06-21 21:03:12 +0900

    [박주형] -post 연습문제2번 풀이 완료

---
+++ src/main/java/com/jooring/servlet/test/ServletTest10Controller.java
@@ -0,0 +1,62 @@
+package com.jooring.servlet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/servlet/test10")
+public class ServletTest10Controller extends HttpServlet{
+	
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		
+		out.println("<html><head><title>사용자정보확인</title></head>");
+		out.println("<body>");
+		
+		
+		if(!id.equals(userMap.get("id"))) {
+			out.println("<h1> id가 일치하지 않습니다.</h1>");
+		}else if(!password.equals(userMap.get("password"))) {
+			out.println("<h1>password가 일치하지않습니다.</h1>");
+		}else {
+			out.println("<h1>"+ userMap.get("name") + "님 환영 합니다</h1>");
+		}
+		
+		out.println("</body></html>");
+		
+	}
+	
+	
+	// doPost 메소드 바깥쪽에 위치
+	private final Map<String, String> userMap =  new HashMap<String, String>() {
+	    {
+	        put("id", "hagulu");
+	        put("password", "asdf");
+	        put("name", "김인규");
+	    }
+	    
+	    
+	};
+	
+	
+	
+	
+
+}
